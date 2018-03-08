ae7a527e6ed8a672aeca29b907391b6ab2bbaa92
Michael Dunham <michaeljwdunham@gmail.com>
2018-03-07 18:48:51 -0700

    Updated to include X Pattern
    X Pattern added

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # S-AR-2018
 Small programming test.
+
+There are two examples requested
+
+
+Tested on: OSX - Python 2.7
+
+Instructions:
+
+- No special modules have been used
+
+To build tree example: run tree.py
+
+To build X Pattern example: run xpattern.py
+
+
---
+++ xpattern.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# # -*- coding: utf-8 -*-
+
+""" Simpple test for Scope AR
+"""
+
+import os
+import colors
+import time
+import sys
+import random
+
+
+def makedax(s):
+	''' This is the factory to make a X, could have went a couple ways but decided to use a list to the make the X.
+	'''
+	for t in range(s):
+	    p = list(" " * s)
+	    p[t] = "*"
+	    p[-(t+1)] = "*"
+	    print " ".join(p)
+
+
+def checkinput(l,h,number):
+
+	if l <= number <= h:
+		pass
+
+	else:
+		print "I said 10-20.... try again"
+		exit()
+
+def words():
+	colors.prGreen("""\n
+   _____                                 _____  
+  / ____|                          /\   |  __ \ 
+ | (___   ___ ___  _ __   ___     /  \  | |__) |
+  \___ \ / __/ _ \| '_ \ / _ \   / /\ \ |  _  / 
+  ____) | (_| (_) | |_) |  __/  / ____ \| | \ \ 
+ |_____/ \___\___/| .__/ \___| /_/    \_\_|  \_\                        
+		  |_|
+  --------------------------------------------
+  Awesome Super Xmas Tree Maker! - Mike Dunham
+
+				  """)
+
+words()
+toolbar_width = 30
+
+# setup toolbar
+sys.stdout.write("Energizing Bananas [%s]" % (" " * toolbar_width))
+sys.stdout.flush()
+sys.stdout.write("\b" * (toolbar_width+1)) # return to start of line, after '['
+
+for i in xrange(toolbar_width):
+    time.sleep(0.1)
+    sys.stdout.write("-")
+    sys.stdout.flush()
+
+sys.stdout.write("\n")
+
+os.system('clear')
+
+print("How big do you want your X?\n(Suggested 5-20): ")
+
+xsize  = int(raw_input().lower())
+sizeresult = checkinput(5,20,xsize)
+
+os.system('clear')
+makedax(xsize)
+colors.prLightPurple("\nEnjoy your X Michael Dunham\n")
+
+
+
+
